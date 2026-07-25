208660bb037793ab28df2bebad4e09e008e375e6
alan7383 <alan.mussot@outlook.fr>
2026-07-26 00:53:08 +0200

    Restore Linux app icon correctly with renamed file to prevent caching issues and bump version to 1.0.9

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 group = "com.alananasss"
-version = "1.0.8-SNAPSHOT"
+version = "1.0.9-SNAPSHOT"
 
 repositories {
     google()
@@ -88,7 +88,7 @@ dependencies {
     implementation("com.github.hypfvieh:dbus-java-core:4.3.1")
     implementation("com.github.hypfvieh:dbus-java-transport-jnr-unixsocket:4.3.1")
 
-    val javafxVersion = "21.0.8"
+    val javafxVersion = "21.0.9"
     val javafxClassifier = when {
         osName.contains("win") -> "win"
         osName.contains("mac") -> "mac"
@@ -120,7 +120,7 @@ compose.desktop {
                 TargetFormat.AppImage
             )
             packageName = "KittyTune"
-            packageVersion = "1.0.8"
+            packageVersion = "1.0.9"
             description = "KittyTuneDesktop"
             vendor = "KittyTune"
 
@@ -154,6 +154,10 @@ compose.desktop {
                 menuGroup = "AudioVideo"
                 appCategory = "AudioVideo"
                 packageName = "kitty-tune"
+                iconFile.set(project.file("src/main/resources/icons/kittytune_linux.png"))
+                
+                
+                
                 
             }
 
