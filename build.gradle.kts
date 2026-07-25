8bf32232d1f2d90fef4e39880822b4cf527014d3
alan7383 <alan.mussot@outlook.fr>
2026-07-26 00:23:41 +0200

    Bump version to 1.0.2 for release trigger

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 group = "com.alananasss"
-version = "1.0.1-SNAPSHOT"
+version = "1.0.2-SNAPSHOT"
 
 repositories {
     google()
@@ -120,7 +120,7 @@ compose.desktop {
                 TargetFormat.AppImage
             )
             packageName = "KittyTune"
-            packageVersion = "1.0.1"
+            packageVersion = "1.0.2"
             description = "KittyTuneDesktop"
             vendor = "KittyTune"
 
