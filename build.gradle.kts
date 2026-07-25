c45fe14ff2b8a0a03e59ae14749bc10bae2f5c85
alan7383 <alan.mussot@outlook.fr>
2026-07-26 00:39:48 +0200

    Force add kittytune.png and bump version to 1.0.7

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 group = "com.alananasss"
-version = "1.0.6-SNAPSHOT"
+version = "1.0.7-SNAPSHOT"
 
 repositories {
     google()
@@ -88,7 +88,7 @@ dependencies {
     implementation("com.github.hypfvieh:dbus-java-core:4.3.1")
     implementation("com.github.hypfvieh:dbus-java-transport-jnr-unixsocket:4.3.1")
 
-    val javafxVersion = "21.0.6"
+    val javafxVersion = "21.0.7"
     val javafxClassifier = when {
         osName.contains("win") -> "win"
         osName.contains("mac") -> "mac"
@@ -120,7 +120,7 @@ compose.desktop {
                 TargetFormat.AppImage
             )
             packageName = "KittyTune"
-            packageVersion = "1.0.6"
+            packageVersion = "1.0.7"
             description = "KittyTuneDesktop"
             vendor = "KittyTune"
 
