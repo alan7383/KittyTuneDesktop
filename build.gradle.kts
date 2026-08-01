c39d3b750dd1d47d1bc6d2cdd3f9e8988cc0c7d1
alan7383 <alan.mussot@outlook.fr>
2026-08-01 21:35:33 +0200

    chore: update compose version and gitignore

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 plugins {
     kotlin("jvm") version "2.4.0"
     kotlin("plugin.serialization") version "2.4.0"
-    id("org.jetbrains.compose") version "1.11.1"
+    id("org.jetbrains.compose") version "1.12.0-beta01"
     id("org.jetbrains.kotlin.plugin.compose") version "2.4.0"
 }
 
