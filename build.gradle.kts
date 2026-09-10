e324807c895f89b7fc129b9aa6f3a6d2b057eab4
alan7383 <alan.mussot@outlook.fr>
2026-09-10 22:54:11 +0200

    chore(release): bump version to 1.3.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 group = "com.alananasss"
-version = "1.3.4"
+version = "1.3.5"
 
 repositories {
     google()
@@ -208,7 +208,7 @@ compose.desktop {
                 TargetFormat.AppImage
             )
             packageName = "KittyTune"
-            packageVersion = "1.3.4"
+            packageVersion = "1.3.5"
             description = "KittyTuneDesktop"
             vendor = "KittyTune"
 
